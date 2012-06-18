06a090d5327c801c80883a5fe35adcf5d38c6f9b
Kimo Rosenbaum <kimor79@yahoo.com>
2012-06-18 12:02:36 -0700

    camelCase

--- tests/phpunit/v2/test_driver.php
+++ tests/phpunit/v2/test_driver.php
@@ -36,67 +36,67 @@ class APIProducerV2DriverTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($expected, $got);
 	}
 
-	public function testvalidateParameter_sortField() {
+	public function testValidateParameter_sortField() {
 		$got = $this->api->validateParameter_sortField('foobar');
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField1() {
+	public function testValidateParameter_sortField1() {
 		$got = $this->api->validateParameter_sortField('foobar1');
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField2() {
+	public function testValidateParameter_sortField2() {
 		$got = $this->api->validateParameter_sortField('foo-bar');
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField3() {
+	public function testValidateParameter_sortField3() {
 		$got = $this->api->validateParameter_sortField('foo_bar');
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField4() {
+	public function testValidateParameter_sortField4() {
 		$got = $this->api->validateParameter_sortField('foo-bar');
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField5() {
+	public function testValidateParameter_sortField5() {
 		$got = $this->api->validateParameter_sortField('foo.bar');
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField6() {
+	public function testValidateParameter_sortField6() {
 		$got = $this->api->validateParameter_sortField('foo:bar');
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField7() {
+	public function testValidateParameter_sortField7() {
 		$got = $this->api->validateParameter_sortField('foo:bar:blaz');
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField8() {
+	public function testValidateParameter_sortField8() {
 		$got = $this->api->validateParameter_sortField('foo bar');
 		$this->assertFalse($got);
 	}
 
-	public function testvalidateParameter_sortField9() {
+	public function testValidateParameter_sortField9() {
 		$got = $this->api->validateParameter_sortField('');
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField10() {
+	public function testValidateParameter_sortField10() {
 		$got = $this->api->validateParameter_sortField(NULL);
 		$this->assertTrue($got);
 	}
 
-	public function testvalidateParameter_sortField11() {
+	public function testValidateParameter_sortField11() {
 		$got = $this->api->validateParameter_sortField(false);
 		$this->assertFalse($got);
 	}
 
-	public function testvalidateParameter_sortField12() {
+	public function testValidateParameter_sortField12() {
 		$got = $this->api->validateParameter_sortField(true);
 		$this->assertFalse($got);
 	}
